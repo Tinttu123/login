b6f32f51e9e5408d0451b818839275413d78564b
Tinttu <tiina.soramies@gmail.com>
2025-03-12 11:21:18 +0200

    lisättiin firebase tiedosto

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAq9BpYJq0YuqaTfUJe6pwgLSKDS-P75MM",
+  authDomain: "login-test-93fe9.firebaseapp.com",
+  projectId: "login-test-93fe9",
+  storageBucket: "login-test-93fe9.firebasestorage.app",
+  messagingSenderId: "576177901710",
+  appId: "1:576177901710:web:edc8cb6936ce9aaeb4b9df"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
